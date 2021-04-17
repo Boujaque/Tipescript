d6aac0b33eb3b13d68b7b4e7d41c0227a56efbca
Boujaque <rafael.constantinescu@gmail.com>
2021-04-17 20:22:49 +0300

    package,json initialized, srvers started

---
+++ Section1_Getting_Started/L9_Understanding_Typescript/app.ts
@@ -0,0 +1,16 @@
+console.log("Your code goes here...!!!");
+
+//installed:
+// > npm init  --> create the package.jason in our folder
+// > npm install --save-dev lite-serv --> instal lite server and added to package.json
+// here we added under scripts :
+//   "start": "lite-server"
+// runt the server with:
+//npm start -->
+//      Local: http://localhost:3000
+// External: http://192.168.0.108:3000
+// after changes and recompilation with :
+//  tsc app.ts
+//loaclhost is automaticaly updated.
+
+
